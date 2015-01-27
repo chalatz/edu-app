1cfc2125a65461731f5bee0ca7c2c009e3c79bac
vagrant <vagrant@scotchbox>
2015-01-27 07:48:54 +0000

    gitignore update

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'edu',
+			'database'  => 'eduapp',
 			'username'  => 'root',
-			'password'  => '123',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
