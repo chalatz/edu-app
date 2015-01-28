9d08ef2e2f7e7ee969a9e177c9cf5986035e1bc9
chalatz <chralatz@gmail.com>
2015-01-28 15:51:53 +0000

    Working on sites

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -36,7 +36,7 @@ class SessionsController extends \BaseController {
         Session::flash('flash_message', '<i class="fa fa-exclamation-triangle"></i> Λανθασμένο email ή κωδικός πρόσβασης');
         Session::flash('alert-class', 'flash-error');
         
-        return Redirect::home();
+        return Redirect::back()->withInput();
         
 	}
     
