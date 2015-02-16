a9ab6bce49e1221e0b5d147d620cb0e47fd5d604
chalatz <chralatz@gmail.com>
2015-02-16 08:57:53 +0000

    Added bar charts

--- app/views/admin/stats.blade.php
+++ app/views/admin/stats.blade.php
@@ -7,6 +7,13 @@
         $districts_count = 0;
     ?>
 
+    <script>
+        var cat_names = [],
+            cat_counts = [],
+            district_names = [],
+            district_counts = [];
+    </script>
+
 	<h1>Στατιστικά</h1>
     <hr>
 
@@ -26,11 +33,21 @@
                     <td>{{ Site::where('cat_id', '=', $cat->id)->count() }}</td>
                 </tr>
                 <?php $cats_count += Site::where('cat_id', '=', $cat->id)->count(); ?>
+            
+                <script>
+                    var cat_name = "{{ $cat->category_name }}",
+                        cat_count = {{ Site::where('cat_id', '=', $cat->id)->count() }};
+                    cat_names.push(cat_name);
+                    cat_counts.push(cat_count);
+                </script>
             @endforeach
         </tbody>
     </table>
 
     <p>Σύνολο: <strong>{{ $cats_count }}</strong></p>
+
+    <div class="ct-chart ct-golden-section cats-bars-chart"></div>
+
     <hr>
 
     <h2>Περιφέρειες</h2>
@@ -49,11 +66,64 @@
                     <td>{{ Site::where('district_id', '=', $district->id)->count() }}</td>
                 </tr>
                 <?php $districts_count += Site::where('district_id', '=', $district->id)->count(); ?>
+            
+                <script>
+                    var district_name = "{{ $district->district_name }}",
+                        district_count = {{ Site::where('district_id', '=', $district->id)->count() }};
+                    district_names.push(district_name);
+                    district_counts.push(district_count);
+                </script>
             @endforeach
         </tbody>
     </table>
 
     <p>Σύνολο: <strong>{{ $districts_count }}</strong></p>
+
+    <div class="ct-chart ct-golden-section districts-bars-chart"></div>
+
     <hr>
+
+    <script>
+        var cats_data = {
+            labels: cat_names,
+            series: [
+            cat_counts
+          ]
+        };
+        
+        var districts_data = {
+            labels: district_names,
+            series: [
+            district_counts
+          ]
+        };
+
+        var options = {
+          seriesBarDistance: 15
+        };
+
+        var responsiveOptions = [
+          ['screen and (min-width: 641px) and (max-width: 1024px)', {
+            seriesBarDistance: 10,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value;
+              }
+            }
+          }],
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+
+        new Chartist.Bar('.cats-bars-chart', cats_data, options, responsiveOptions);
+        new Chartist.Bar('.districts-bars-chart', districts_data, options, responsiveOptions);
+        
+    </script>
 
 @stop
