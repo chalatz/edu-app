76476398f29081a8fbdf1f9ac54581142eedd9d6
chalatz <chralatz@gmail.com>
2015-03-30 20:10:00 +0000

    granted site create to another

--- app/controllers/SitesController.php
+++ app/controllers/SitesController.php
@@ -27,7 +27,7 @@ class SitesController extends \BaseController {
         // Disable sites creation
         Session::flash('flash_message', '<i class="fa fa-exclamation"></i> Η υποβολή υποψηφιοτήτων έχει λήξει.');
         Session::flash('alert-class', 'flash-info');
-        if(Auth::user()->id != 59 && Auth::user()->id != 273){
+        if(Auth::user()->id != 59 && Auth::user()->id != 273 && Auth::user()->id != 160 ){
             return Redirect::home();
         }
         
