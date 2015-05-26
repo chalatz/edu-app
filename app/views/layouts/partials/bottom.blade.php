003eccf40cdc09daf3a92ea1c3b5f0c45352a954
chalatz <chralatz@gmail.com>
2015-05-26 08:42:11 +0000

    added stergou credit

--- app/views/layouts/partials/bottom.blade.php
+++ app/views/layouts/partials/bottom.blade.php
@@ -16,12 +16,16 @@
 	</div>
 
 	<div class="pure-g">
-	    <div class="pure-u-1 pure-u-md-1-1">
+	    <div class="pure-u-1 pure-u-md-1-2">
 	    	<p>
 	    		<i class="fa fa-cogs"></i> Ανάπτυξη - Σχεδίαση: <a href="http://www.chalatz.mysch.gr" target="_blank">Αλατζάς Χρήστος</a>
 	    	</p>
 	    </div>
-
+	    <div class="pure-u-1 pure-u-md-1-2">
+	    	<p>
+	    		Σχεδίαση αλγορίθμου αναθέσεων: <a href="https://github.com/sstergou" target="_blank">Στέργου Ευστάθιος</a>
+	    	</p>
+	    </div>
 	</div>
 
 </div>
